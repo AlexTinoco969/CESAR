1a526feb47dce4fe5dc68d8efd00a74cb7f12ec8
AlexTinoc969 <chaplinslife@gmail.com>
2020-12-17 09:17:52 +0100

    fix()resuelto?

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/2', function () {
+
+Route::get('/1', function () {
+
     return view('holabuenas');
 });
 //hello there
